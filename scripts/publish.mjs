973e139afb0690b0ed60034244af02812febd7ba
bindernews <bindernews@gmail.com>
2022-03-31 10:59:23 -0400

    Remove source map from published version

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -3,6 +3,7 @@ import { execSync } from "node:child_process";
 (async () => {
   await rm("./public", { recursive: true });
   execSync("npm run build");
+  await rm("./public/build/bundle.js.map");
   execSync("git switch gh-pages");
   await rm("./docs", { recursive: true });
   await rename("./public", "./docs");
