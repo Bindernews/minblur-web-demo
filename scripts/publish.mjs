07111503f27b6cd43e734f5af1dd4e8548a6dcc3
bindernews <bindernews@gmail.com>
2022-03-31 10:55:52 -0400

    Add publish script to gh-pages branch as well

---
+++ scripts/publish.mjs
@@ -0,0 +1,9 @@
+import { rename, rm } from "node:fs/promises";
+import { execSync } from "node:child_process";
+(async () => {
+  await rm("./public", { recursive: true });
+  execSync("npm run build");
+  execSync("git switch gh-pages");
+  await rm("./docs", { recursive: true });
+  await rename("./public", "./docs");
+})();
